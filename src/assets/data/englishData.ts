5064de06ca3969e5fe5fa63e99579d7dcb690287
bori_mix <bori.mix@yandex.ru>
2025-02-03 21:41:49 +0300

    info update (exp)

--- src/assets/data/englishData.ts
+++ src/assets/data/englishData.ts
@@ -1,5 +1,5 @@
 export const englishData = {
-    experience: 'Experience 2 years',
+    experience: 'Experience > 2 years',
     personalInformation: `I\`m a Frontend developer with a secondary technical education and currently pursuing higher education. I would be happy to discuss how I can contribute to your team and business development!`,
     contacts: {
         address: 'Russia, Bataysk',
